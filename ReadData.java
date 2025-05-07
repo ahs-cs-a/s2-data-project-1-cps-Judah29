a2a348d00cab5d28e64e4f4e9238344f38f30057
AHS CS <ahs-cs@ausdk12.org>
2025-05-07 10:06:28 -0700

    gjfc

--- ReadData.java
+++ ReadData.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class ReadData-Student{
     //I hard-coded the number of rows and columns so 
     //I could use a 2D array
-    private double[][] data = new double[...][...];
+    private double[][] data = new double[21908][14];
 
     //This should read in the csv file and store the data in a 2D array,
     //data -- don't forget to skip the header line and parse everything
@@ -18,7 +18,8 @@ public class ReadData-Student{
             while(scanner.hasNextLine()){
                 String line = scanner.nextLine();
                 String[] lineArr = line.split(",");
-                ...
+                for(int col = 0; col < data[0].length; col++){
+                    data[row][col] = Double.parseDouble(lineArr[col]);
                 row++;
             }
             scanner.close();
@@ -35,8 +36,10 @@ public class ReadData-Student{
     //this should return a double array of the column
     //of data
     public double[] getColumn(int col){
-        double[] column = ...
-        ...
+        double[] column = new double[data.length]; 
+        for(int i = 0; i < column.length; i++){
+             column[i] = data[i][col];
+             } 
         return column;
     }
 
@@ -48,31 +51,36 @@ public class ReadData-Student{
     //divided by the number of values - 1(sample variance)
     //Use Math.pow to square the difference
     //and Math.sqrt to take the square root
-    public double stdDeviation(double[] arr){
-        double sum = 0;
-        double mean = ...
-        ...
-        return .. //sample variance!
-    }
+    public double stdDeviation(double[] arr){ 
+        double sum = 0; double mean = mean(arr); 
+        for(int x = 0; x < arr.length; x++){
+             sum += Math.pow(arr[x]-mean, 2); 
+             } 
+             sum = sum / (arr.length-1); 
+        return Math.sqrt(sum); 
+        }
     
     //this returns the mean of the column of data passed in
     //the mean is the sum of the values divided by the number of values
     public double mean(double[] arr){
-        double sum = 0;
-        ...
-        return ...;
+        double sum = 0; for(int i = 0; i < arr.length; i++){ 
+            sum += arr[i]; 
+            } 
+        return sum/arr.length; 
+        }
     }
 
     //this returns the values of a column in standard units
     //the standard units are the value minus the mean divided by the standard deviation
     //this should return a double array of the standard units
-    public double[] standardUnits(double[] arr){
-        double[] stdArr = ...
-        double stdDeviation = ...;
-        double mean = ...;
-        ...
+    public double[] standardUnits(double[] arr){ 
+        double[] stdArr = new double[arr.length]; 
+        double stdDeviation = stdDeviation(arr); double mean = mean(arr); 
+        for(int i = 0; i < arr.length; i++){
+             stdArr[i] = (arr[i]-mean)/stdDeviation;
+              } 
         return stdArr;
-    }
+         }
     
     //this returns the correlation between the two columns of data passed in
     //the correlation is the sum of the products of the standard units
@@ -82,10 +90,15 @@ public class ReadData-Student{
     //between the two columns of data
     //the correlation is between -1 and 1
     public double correlation(double[] x, double[] y){
-        double sum = 0;
-        ...
-        return ...;    
-    }
+         double sum = 0; double[] xStandard = new double[x.length];
+          double[] yStandard = new double[y.length];
+           xStandard = standardUnits(x); 
+           yStandard = standardUnits(y); 
+           for(int i = 0; i < x.length; i++){ 
+                sum += xStandard[i] * yStandard[i]; 
+                } 
+           return sum/(x.length-1); 
+           }
     
     public void runRegression(){
         // double[] x = getColumn(7);
